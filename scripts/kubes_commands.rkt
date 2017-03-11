17aa13c155123e0d693f67132c5d602f08d5a52b
Alex Angelini <alex.louis.angelini@gmail.com>
2017-03-10 22:53:06 -0500

    Add kubes connect command

---
+++ scripts/kubes_commands.rkt
@@ -0,0 +1,43 @@
+#!/usr/bin/env racket
+
+#lang racket
+
+(require "../lib/constants.rkt"
+         "../lib/exec.rkt")
+
+(define (pods-list namespace [app #f])
+  (define pod-filter (if app (format "app=~a" app) ""))
+  (string-split
+   (exec-stdout root-dir
+                "kubectl" "get" "pods"
+                "--namespace" namespace
+                "-l" pod-filter
+                "-o" "jsonpath"
+                "--template" "{.items[*].metadata.name}")))
+
+(define (connect-to-pod namespace [app #f])
+  (define pod (car (pods-list namespace app)))
+  (format "kubectl --namespace=~a exec -it ~a -- bash" namespace pod))
+
+(define (print-usage)
+  (display "bin/kubes allows you to interact with a runnings kubes project
+
+Usage:
+  kubes [command]
+
+Available Commands:
+  connect    Connect to a bash shell on a random running pod
+"))
+
+(define (run)
+  (define cli-args (vector->list (current-command-line-arguments)))
+  (define-values (command args) (if (empty? cli-args)
+                                    (values "" '())
+                                    (values (car cli-args)
+                                            (cdr cli-args))))
+  (case command
+    [("connect") (apply connect-to-pod args)]
+    [else (begin (print-usage)
+                 (exit 1))]))
+
+(run)

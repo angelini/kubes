be1ea32cfcc8bb9a1d15b286a9adb16243f55638
Alex Angelini <alex.louis.angelini@gmail.com>
2017-03-23 20:24:15 -0400

    Fix spark-master connectivity

--- templates/start_spark_master.sh
+++ templates/start_spark_master.sh
@@ -3,7 +3,7 @@
 set -x
 
 unset SPARK_MASTER_PORT
-spark/sbin/start-master.sh
+spark/sbin/start-master.sh -h 0.0.0.0 --properties-file spark.conf
 
 find spark/logs -name "*$(hostname)*.out" |
   xargs tail --pid "$(find /tmp -name '*.pid' | xargs cat)" -f
